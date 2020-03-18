7d1461463c200266c50e9977b2b1480e71f1165a
vkoba <vkoba@phoenixit.ru>
2020-03-18 19:16:13 +0300

    fix manager

--- src/main/java/ru/liga/pattern/task1/entity/Manager.java
+++ src/main/java/ru/liga/pattern/task1/entity/Manager.java
@@ -1,6 +1,6 @@
 package ru.liga.pattern.task1.entity;
 
-public  class Manager {
+public  class Manager extends Employee {
     public boolean checkCreditRequest(CreditRequest creditRequest){
         throw new UnsupportedOperationException("Придумайте свою логику выдачи кредита :)");
     };
